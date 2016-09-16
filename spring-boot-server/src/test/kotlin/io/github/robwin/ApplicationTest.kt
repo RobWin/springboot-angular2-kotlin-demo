2325bc0347b48325f798aa2b7ff9bca28950c0e2
Robert Winkler <rwinkler@telekom.de>
2016-09-16 12:33:17 +0200

    Added gradle.properties file

--- spring-boot-server/src/test/kotlin/io/github/robwin/ApplicationTest.kt
+++ spring-boot-server/src/test/kotlin/io/github/robwin/ApplicationTest.kt
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
-class SpringbootAngular2KotlinDemoApplicationTests {
+class ApplicationTest {
 
 	@Test
 	fun contextLoads() {
